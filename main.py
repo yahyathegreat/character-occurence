2ac6657051bba5614caa5e90ecbe707260b505ac
yahyathegreat <yahyamardan1234@gmail.com>
2024-10-11 20:15:38 +0500

    mid product

--- main.py
+++ main.py
@@ -17,3 +17,13 @@ for num in range(lower, upper + 1):
                 break
         else:
                 print(num)
+num = int(input("enter a number : "))
+t = num
+numlen = 0
+while t>0:
+    numlen = numlen+1
+    t = int(t/10)
+    if numlen>=4:
+         numlen = int(numlen/2)
+         chk = 0
+         while num>0:
