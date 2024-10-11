76587dd6d9676c3099d61f54863dd8fd7fdcfa47
yahyathegreat <yahyamardan1234@gmail.com>
2024-10-11 19:53:06 +0500

    character occurence

---
+++ main.py
@@ -0,0 +1,9 @@
+string = input("please enter your own word: ")
+char = input("please enter your own character")
+i = 0
+count = 0
+while (i < len(string)):
+    if(string[i] == char):
+        count = count + 1
+    i = i + 1
+print("the total number of times ", char, "has occured=" ,count)
