3bee57cd958fd5db033c03137d8092ca81f0ae3e
yahyathegreat <yahyamardan1234@gmail.com>
2024-10-11 20:06:24 +0500

    is this a prime number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 string = input("please enter your own word: ")
-char = input("please enter your own character")
+char = input("please enetr your own character")
 i = 0
 count = 0
 while (i < len(string)):
@@ -7,3 +7,13 @@ while (i < len(string)):
         count = count + 1
     i = i + 1
 print("the total number of times ", char, "has occured=" ,count)
+lower = int(input("enter a upper range: "))
+upper = int(input("enter a lower range: "))
+print("prime numbers between", lower, "and", upper, "are:")
+for num in range(lower, upper + 1):
+    if num > 1:
+        for i in range(2, num):
+            if ( num % i) == 0:
+                break
+        else:
+                print(num)
